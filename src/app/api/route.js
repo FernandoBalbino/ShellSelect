f73c45ae97fc1f51098a59bdd2eca3b18874fa00
Fernando Balbino <dev.fernandobalbino@gmail.com>
2024-11-07 10:10:24 -0300

    Updates Api

--- src/app/api/route.js
+++
@@ -1,7 +0,0 @@
-import { data } from "../ferramentas/data"; // Ajuste o caminho conforme necessário
-
-export async function GET(request) {
-    return new Response(JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' }
-    });
-}
